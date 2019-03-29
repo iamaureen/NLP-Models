498803695dbf48179b8ef8b544ae0d0e73ee391c
iamaureen <ishrat.ahmed@asu.edu>
2019-03-29 16:21:51 -0400

    added lstm model

--- word embeddings/glove_keras.py
+++ word embeddings/glove_keras.py
@@ -5,9 +5,8 @@ from numpy import zeros
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
 from keras.layers import Embedding
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
 # define documents
 docs = ['Well done!',
 		'Good work',
@@ -48,18 +47,36 @@ for word, i in t.word_index.items():
 	embedding_vector = embeddings_index.get(word)
 	if embedding_vector is not None:
 		embedding_matrix[i] = embedding_vector
+
 # define model
-model = Sequential()
-e = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=4, trainable=False)
-model.add(e)
-model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
-# compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-# summarize the model
-print(model.summary())
-# fit the model
-model.fit(padded_docs, labels, epochs=50, verbose=0)
+# model = Sequential()
+# e = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=4, trainable=False)
+# model.add(e)
+# model.add(Flatten())
+# model.add(Dense(1, activation='sigmoid'))
+# # compile the model
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+# # summarize the model
+# print(model.summary())
+# # fit the model
+# model.fit(padded_docs, labels, epochs=50, verbose=0)
+
 # evaluate the model
-loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
+# loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
+# print('Accuracy: %f' % (accuracy*100))
+
+## create the LSTM model
+model_lstm = Sequential()
+model_lstm.add(Embedding(vocab_size, 100, input_length=4, weights=[embedding_matrix], trainable=False))
+# model_lstm.add(Dropout(0.2))
+# model_lstm.add(Conv1D(64, 5, activation='relu'))
+# model_lstm.add(MaxPooling1D(pool_size=4))
+model_lstm.add(LSTM(100))
+model_lstm.add(Dense(1, activation='sigmoid'))
+model_lstm.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+## Fit train data
+model_lstm.fit(padded_docs, labels, validation_split=0.4, epochs = 3)
+
+# evaluate the model
+loss, accuracy = model_lstm.evaluate(padded_docs, labels, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
